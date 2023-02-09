8d153d97158e1759e09e687ee14413be71ae76d7
ValentinaRippe <lauravalentina200102@gmail.com>
2023-02-09 13:22:42 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors")
+
+const PORT = 3030;
+const app = express();
+mongoose.set('strictQuery', true);
+
+const todoRoutes = require("./routes/todoRoutes")
+const connectionOptions = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+};
+
+app.use(express.json());
+app.use(cors())
+
+mongoose.connect("mongodb://localhost/todolist", connectionOptions);
+const connection = mongoose.connection
+
+connection.once('open', () => {
+    console.log('Db is connected')
+})
+
+app.use("/todos", todoRoutes);
+
+
+app.listen(PORT, () => {
+    console.log("The server is listening on port " + PORT)
+})
+
